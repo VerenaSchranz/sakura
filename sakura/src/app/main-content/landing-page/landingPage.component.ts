b070ffc1f5c94c98b4b2a765cdce1ac33bd3cae4
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-04 12:27:53 +0200

    update

--- sakura/src/app/main-content/landing-page/landingPage.component.ts
+++ sakura/src/app/main-content/landing-page/landingPage.component.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
-    selector: 'app-landPage',
+    selector: 'app-landingPage',
     standalone: true,
     imports: [CommonModule, NavbarComponent],
     template: `
     <section>
-        <app-navbar></app-navbar>
+        <div><app-navbar></app-navbar></div>
         <h1 class="fontRaleway">SAKURA RAMEN</h1>
         <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
     </section>
---
+++ sakura/src/app/main-content/main-content.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landing-page/landingPage.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [CommonModule, 
+    LandingPageComponent, 
+    OurMenuComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss',
+})
+export class MainContentComponent {}
